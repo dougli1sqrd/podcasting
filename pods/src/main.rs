28eb39c3d332d8cd3551d53ed826076db47ea8ce
Eric Douglass <dougli1sqrd@gmail.com>
2023-07-05 19:45:16 -0700

    podcasts are now subscribable and metadata gets saved in mem db

--- pods/src/main.rs
+++ pods/src/main.rs
@@ -1,7 +1,7 @@
-use std::{collections::HashMap, sync::{Arc}, str::FromStr};
+use std::{collections::HashMap, str::FromStr, sync::Arc};
 
 use axum::{
-    extract::{State, Path},
+    extract::{Path, State},
     http::{StatusCode, Uri},
     response::IntoResponse,
     routing::{get, post},
@@ -13,17 +13,18 @@ use uuid::Uuid;
 
 #[derive(Clone)]
 struct AppState<D: DB> {
-    current_user: Option<User>,
+    current_user: Option<Uuid>,
     db: D,
 }
 
 fn routes() -> Router<Arc<Mutex<AppState<InMemoryStore>>>> {
-    Router::new().route("/", get(handler))
+    Router::new()
+        .route("/", get(handler))
         .route("/users", post(add_user))
         .route("/users/:id", get(get_user))
         .route("/login", get(user_status))
         .route("/login/:id", post(login))
-        .route("/subscribe", post(subscribe_to_podcast))
+        .route("/podcast", post(subscribe_to_podcast))
 }
 
 #[tokio::main]
@@ -49,76 +50,144 @@ async fn add_user<D: DB>(
     State(state): State<Arc<Mutex<AppState<D>>>>,
     Json(payload): Json<CreateUser>,
 ) -> impl IntoResponse {
-    
     let user = state.lock().await.db.create_user(payload).unwrap();
     // Presumably store somewhere?
     (StatusCode::CREATED, Json(user))
 }
 
-async fn get_user<D: DB>(State(state): State<Arc<Mutex<AppState<D>>>>, Path(uid): Path<Uuid>) -> impl IntoResponse {
+async fn get_user<D: DB>(
+    State(state): State<Arc<Mutex<AppState<D>>>>,
+    Path(uid): Path<Uuid>,
+) -> impl IntoResponse {
     let x = state.lock().await.db.get_user(uid);
     // (StatusCode::OK, Json(x))
     match x {
         Ok(u) => (StatusCode::OK, Json(Some(u))),
         Err(Error::NotFound) => (StatusCode::NOT_FOUND, Json(None)),
-        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(None))
+        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, Json(None)),
     }
 }
 
-async fn login<D: DB>(State(state): State<Arc<Mutex<AppState<D>>>>, Path(uid): Path<Uuid>) -> impl IntoResponse {
+async fn login<D: DB>(
+    State(state): State<Arc<Mutex<AppState<D>>>>,
+    Path(uid): Path<Uuid>,
+) -> impl IntoResponse {
     let mut s = state.lock().await;
     match s.db.get_user(uid) {
         Ok(u) => {
-            s.current_user = Some(u);
+            s.current_user = Some(u.id);
             StatusCode::OK
-        },
-        Err(_) => StatusCode::NOT_EXTENDED
+        }
+        Err(_) => StatusCode::NOT_EXTENDED,
     }
 }
 
 #[derive(Serialize, Clone, Debug)]
 struct UserStatus {
-    user: Option<User>,
+    user: Option<Uuid>,
     logged_in: bool,
 }
 
 async fn user_status<D: DB>(State(state): State<Arc<Mutex<AppState<D>>>>) -> impl IntoResponse {
     match &state.lock().await.current_user {
-        Some(u) => {
-            Json(UserStatus {
-                user: Some(u.clone()),
-                logged_in: true
-            })
-        },
-        None => Json(UserStatus { user: None, logged_in: false })
+        Some(u) => Json(UserStatus {
+            user: Some(u.clone()),
+            logged_in: true,
+        }),
+        None => Json(UserStatus {
+            user: None,
+            logged_in: false,
+        }),
     }
 }
 
-async fn subscribe_to_podcast<D: DB>(State(_): State<Arc<Mutex<AppState<D>>>>, Json(rss): Json<PodcastRSS>) -> impl IntoResponse {
+async fn subscribe_to_podcast<D: DB>(
+    State(state): State<Arc<Mutex<AppState<D>>>>,
+    Json(rss): Json<PodcastRSS>,
+) -> impl IntoResponse {
     match Uri::from_str(&rss.rss) {
         Ok(url) => {
-            let resp = reqwest::get(url.to_string()).await.unwrap().text().await.unwrap();
-            let xml = roxmltree::Document::parse(&resp).unwrap();
-            let rss = xml.root().children().find(|n| n.tag_name().name() == "rss").unwrap();
-            let channel = rss.children().find(|n| n.tag_name().name() == "channel").unwrap();
-            let title = channel.children().find(|n| n.tag_name().name() == "title").unwrap().text().unwrap();
-            let description = channel.children().find(|n| n.tag_name().name() == "description").unwrap().text().unwrap();
-            (StatusCode::OK, Json(Some(PodcastChannel {
-                id: Uuid::new_v4(),
-                name: title.to_string(),
-                description: description.to_string(),
-                rss: url.to_string()
-            })))
-        },
+            let state = &mut state.lock().await;
+            let logged_in = state.current_user.clone();
+            let db = &mut state.db;
+
+            match db.get_podcast(rss.rss) {
+                Ok(p) => {
+                    if let Some(u) = logged_in {
+                        let subs = db.subscribe(u, p.rss);
+                        match subs {
+                            Ok(s) => (StatusCode::CREATED, Json(Some(s))),
+                            Err(_) => (StatusCode::BAD_REQUEST, Json(None))
+                        }
+                    } else {
+                        (StatusCode::NOT_FOUND, Json(None))
+                    }
+                },
+                Err(Error::NotFound) => {
+                    // Podcast not found, so let's create it
+                    let (title, description) = parse_rss(url.to_string()).await;
+                    match db.create_podcast(url.to_string(), title, description) {
+                        Ok(p) => {
+                            if let Some(u) = logged_in {
+                                let subs = db.subscribe(u, p.rss);
+                                match subs {
+                                    Ok(s) => (StatusCode::CREATED, Json(Some(s))),
+                                    Err(_) => (StatusCode::BAD_REQUEST, Json(None))
+                                }
+                            } else {
+                                (StatusCode::NOT_FOUND, Json(None))
+                            }
+                        },
+                        Err(_) => (StatusCode::BAD_REQUEST, Json(None))
+                    }
+                },
+                Err(_) => (StatusCode::BAD_REQUEST, Json(None))
+            }
+
+        }
         // try with https://revolutionspodcast.libsyn.com/rss/
-        Err(_) => (StatusCode::BAD_REQUEST, Json(None))
+        Err(_) => (StatusCode::BAD_REQUEST, Json(None)),
     }
+}
+
+async fn parse_rss(rss_url: String) -> (String, String) {
+    let resp = reqwest::get(rss_url)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    let xml = roxmltree::Document::parse(&resp).unwrap();
+    let rss = xml
+        .root()
+        .children()
+        .find(|n| n.tag_name().name() == "rss")
+        .unwrap();
+    let channel = rss
+        .children()
+        .find(|n| n.tag_name().name() == "channel")
+        .unwrap();
+    let title = channel
+        .children()
+        .find(|n| n.tag_name().name() == "title")
+        .unwrap()
+        .text()
+        .unwrap();
+    let description = channel
+        .children()
+        .find(|n| n.tag_name().name() == "description")
+        .unwrap()
+        .text()
+        .unwrap();
+
+    (title.to_string(), description.to_string())
 }
 
 #[derive(Serialize, Clone, Debug)]
 struct User {
     name: String,
-    id: uuid::Uuid,
+    id: Uuid,
+    subscribed: Vec<String>,
 }
 
 #[derive(Serialize, Clone, Debug)]
@@ -131,7 +200,7 @@ struct PodcastChannel {
 
 #[derive(Debug, Deserialize, Clone)]
 struct PodcastRSS {
-    rss: String
+    rss: String,
 }
 
 #[derive(Deserialize)]
@@ -149,17 +218,30 @@ trait DB {
     fn get_user(&self, id: Uuid) -> Result<User, Error>;
 
     fn create_user(&mut self, name: CreateUser) -> Result<User, Error>;
+
+    fn get_podcast(&self, rss: String) -> Result<PodcastChannel, Error>;
+
+    fn create_podcast(
+        &mut self,
+        rss: String,
+        title: String,
+        description: String,
+    ) -> Result<PodcastChannel, Error>;
+
+    fn subscribe(&mut self, user: Uuid, rss: String) -> Result<Vec<String>, Error>;
 }
 
 #[derive(Debug, Clone)]
 struct InMemoryStore {
     users: HashMap<Uuid, User>,
+    podcasts: HashMap<String, PodcastChannel>,
 }
 
 impl InMemoryStore {
     fn new() -> InMemoryStore {
         InMemoryStore {
             users: HashMap::new(),
+            podcasts: HashMap::new(),
         }
     }
 }
@@ -174,8 +256,39 @@ impl DB for InMemoryStore {
         let u = User {
             name: user.name,
             id: uuid,
+            subscribed: vec![],
         };
         let _ = self.users.insert(uuid, u.clone());
         Ok(u)
+    }
+
+    fn get_podcast(&self, rss: String) -> Result<PodcastChannel, Error> {
+        self.podcasts.get(&rss).cloned().ok_or(Error::NotFound)
+    }
+
+    fn create_podcast(
+        &mut self,
+        rss: String,
+        title: String,
+        description: String,
+    ) -> Result<PodcastChannel, Error> {
+        let id = Uuid::new_v4();
+        let p = PodcastChannel {
+            rss: rss.clone(),
+            name: title,
+            description,
+            id,
+        };
+        let _ = self.podcasts.insert(rss, p.clone());
+        Ok(p)
+    }
+
+    fn subscribe(&mut self, user: Uuid, rss: String) -> Result<Vec<String>, Error> {
+        let p = self.get_podcast(rss)?;
+        let u = self.users.get_mut(&user).ok_or(Error::NotFound)?;
+        if !u.subscribed.contains(&p.rss) {
+            u.subscribed.push(p.rss.clone());
+        }
+        Ok(u.subscribed.clone())
     }
 }
